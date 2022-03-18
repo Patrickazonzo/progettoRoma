4b87875b385ba6de13b445c352c569a2ce5e3100
Betacom <Betacom@Betacom-PCBBNW.wind3.hub>
2022-03-18 10:24:07 +0100

    Modifiche Cartella SQL

---
+++ project/SQL/drop.sql
@@ -0,0 +1,5 @@
+drop table admin on delete cascade;
+drop table corso on delete cascade;
+drop table corsista on delete cascade;
+drop table corsistacorso on delete cascade;
+drop table docente on delete cascade;
---
+++ project/SQL/insert.sql
@@ -0,0 +1,9 @@
+insert into admin values (1,'Mario','Rossi');
+insert into admin values (2,'Luigi','Verdi');
+
+insert into docente values (1,'Mario','Bianchi','/cvmario');
+insert into docente values (2,'Gianni','Neri','/cvgianni');
+insert into docente values (3,'Paolo','Belloni','/cvpaolo');
+
+
+insert into corso values (1,'Analisi',10-GEN-2022,11-FEB-2022,2000.00,'Corso di Analisi matematica','1','1');
