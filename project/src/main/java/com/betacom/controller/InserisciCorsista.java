1b78ad261d36a20d4ad09c2c3d695eae82a65d1b
Gennaro Riccardo <Betacom@Betacom-PC5M9Q.station>
2022-03-23 19:14:03 +0100

    Creati servlet Inserisci Corsista, DocenteBC

---
+++ project/src/main/java/com/betacom/controller/InserisciCorsista.java
@@ -0,0 +1,98 @@
+package com.betacom.controller;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.betacom.architecture.dao.DAOException;
+import com.betacom.businesscomponent.facade.AdminFacade;
+import com.betacom.businesscomponent.idgenerator.CorsistaIdGenerator;
+import com.betacom.businesscomponent.model.Corsista;
+import com.betacom.businesscomponent.model.CorsistaCorso;
+import com.betacom.businesscomponent.model.Corso;
+import com.betacom.businesscomponent.utility.DateUtility;
+
+
+@WebServlet("/inseriscicorsista")
+public class InserisciCorsista extends HttpServlet {
+	
+	 private static final long serialVersionUID = 4975300134006616204L; 
+	 
+	 protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	   throws ServletException, IOException { 
+	  AdminFacade aF = AdminFacade.getInstance(); 
+	  Corsista corsista = new Corsista(); 
+	  CorsistaCorso cc = new CorsistaCorso(); 
+	  Corso c = new Corso(); 
+	 
+	  try { 
+	 
+	   corsista.setCodCorsista(CorsistaIdGenerator.getInstance().getNextId()); 
+	 
+	   boolean verificanome = Pattern.matches(request.getParameter("nomecorsista"), "[a-zA-Z]"); 
+	   if (request.getParameter("nomecorsista") != null && request.getParameter("nomecorsista").length() <= 30 
+	     && verificanome == true) { 
+	    corsista.setNomeCorsista(request.getParameter("nomecorsista")); 
+	   } 
+	 
+	   boolean verificacognome = Pattern.matches(request.getParameter("cognomecorsista"), "[a-zA-Z]"); 
+	   if (request.getParameter("cognomecorsista") != null 
+	     && request.getParameter("cognomecorsista").length() <= 30 && verificacognome == true) { 
+	    corsista.setCognomeCorsista(request.getParameter("cognomecorsista")); 
+	   } 
+	   corsista.setPrecedentiFormativi(Byte.parseByte(request.getParameter("precedentiformativi"))); 
+	 
+	   boolean verificacorso = Pattern.matches(request.getParameter("nomecorso"), "[a-zA-Z]"); 
+	   Corso[] nomecorsi = aF.getAllCorsi(); 
+	   if (request.getParameter("nomecorso") != null && request.getParameter("nomecorso").length() <= 30 
+	     && verificacorso == true) { 
+	    for (Corso corso : nomecorsi) { 
+	     if ((request.getParameter("nomecorso").toLowerCase()).equals(corso.getNomeCorso().toLowerCase())) { 
+	      cc.setCodCorso(c.getCodCorso()); 
+	     } 
+	    } 
+	 
+	   } else { 
+	    response.sendRedirect("#"); 
+	 
+	   } 
+	 
+	   if (request.getParameter("dataInizioCorso") == null 
+	     && DateUtility.isValid(request.getParameter("dataInizioCorso")) == false) { 
+	    response.sendRedirect("#"); 
+	   } 
+	   if (request.getParameter("dataFineCorso") == null 
+	     && DateUtility.isValid(request.getParameter("dataFineCorso")) == false) { 
+	    response.sendRedirect("#"); 
+	   } 
+	   if (Integer.parseInt(request.getParameter("dataFineCorso")) 
+	     - Integer.parseInt(request.getParameter("dataInizioCorso")) < 2) 
+	    response.sendRedirect("#"); 
+	 
+	   if (request.getParameter("commenti").length() > 200) 
+	    response.sendRedirect("#"); 
+	 
+	   boolean verificaAula = Pattern.matches(request.getParameter("aulaCorso"), "\\w"); 
+	   if (request.getParameter("aulaCorso").length() > 30 && verificaAula == false) 
+	    response.sendRedirect("#"); 
+	 
+	   if (request.getParameter("docente") == null || request.getParameter("docente") 
+	     .equals(aF.getNominativoDocente(request.getParameter("nomecorso")))) 
+	    response.sendRedirect("#"); 
+	 
+	   aF.createCorsista(corsista); 
+	   aF.createCorsistaCorso(cc); 
+	 
+	   response.sendRedirect("basedati.jsp"); 
+	  } catch (DAOException | ClassNotFoundException exc) { 
+	   exc.printStackTrace(); 
+	   throw new ServletException(exc.getMessage()); 
+	  } 
+	 } 
+	}
+
