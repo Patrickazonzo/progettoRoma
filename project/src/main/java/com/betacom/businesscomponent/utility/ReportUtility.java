1d71352817cbf51bc330372b4de74d122abec423
riccardo <Betacom@Betacom-PC8M4J>
2022-03-25 09:35:59 +0100

    aggiornata report utility visualizza report

--- project/src/main/java/com/betacom/businesscomponent/utility/ReportUtility.java
+++ project/src/main/java/com/betacom/businesscomponent/utility/ReportUtility.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
 import java.util.Vector;
 
 import com.betacom.architecture.dao.DAOConstants;
@@ -13,34 +14,87 @@ import com.betacom.architecture.dao.DAOException;
 import com.betacom.architecture.dbaccess.DBAccess;
 
 public class ReportUtility implements DAOConstants {
-	
+
 	private Connection conn;
-	
-	
+
 	public ReportUtility() throws DAOException, IOException, ClassNotFoundException {
 		conn = DBAccess.getConnection();
 	}
+
 	public Vector<String[]> getReport() throws DAOException {
 		try {
 			Statement stmt = conn.createStatement(
 			ResultSet.TYPE_SCROLL_INSENSITIVE,
 			ResultSet.CONCUR_READ_ONLY);
-			ResultSet rs = stmt.executeQuery(SELECT_REPORT);
-			ResultSetMetaData meta = rs.getMetaData();
-			int numeroColonne = meta.getColumnCount();
+			
+			
+			ResultSet rs1 = stmt.executeQuery(SELECT_TOT_CORSISTI);
+			stmt.close();
+
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs2 = stmt.executeQuery(SELECT_CORSO_PIU_FREQUE);
+			stmt.close();
+
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs3 = stmt.executeQuery(SELECT_DATA_ULTIMO_CORSO);
+			stmt.close();
+
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs4 = stmt.executeQuery(SELECT_DURATA_MEDIA_CORSO);
+			stmt.close();
+
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs5 = stmt.executeQuery(SELECT_NUM_COMMENTI);
+			stmt.close();
+
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs6 = stmt.executeQuery(SELECT_ELENCO_CORSISTI);
+			stmt.close();
+			
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs7 = stmt.executeQuery(SELECT_DOC_CORSO);
+			stmt.close();
+			
+			stmt = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			ResultSet rs8 = stmt.executeQuery(SELECT_POSTI_LIBERI);
+			stmt.close();
+			
+			
+			ResultSetMetaData meta = rs6.getMetaData();
+			int numColonne = meta.getColumnCount();
+
+			String corsisti = rs1.getString(1);
+			String corso = rs2.getString(1);
+			String data = rs3.getString(1);
+			String media = rs4.getString(1);
+			String commenti = rs5.getString(1);
 			Vector<String[]> dati = new Vector<String[]>();
-			rs.beforeFirst();
-			while (rs.next()) {
-				String[] riga = new String[numeroColonne];
-				for (int i = 0; i < numeroColonne; i++)
-					riga[i] = rs.getString(i + 1);
-				dati.add(riga);
+			rs6.beforeFirst();
+			while (rs6.next()) {
+				String[] riga = new String[numColonne];
+				for (int i = 0; i < numColonne; i++) {
+					riga[i] = rs6.getString(i + 1);
+					dati.add(riga);
+				}
+			}
+			String docente = rs7.getString(1);
+			String posti = rs8.getString(1);
+			dati.addAll(corsisti);
+			
+			rs1.close();
+			stmt.close();
+			rs2.close();
+			rs3.close();
+			rs4.close();
+			rs5.close();
+			rs6.close();
+			rs7.close();
+			rs8.close();
 			}
 			return dati;
-		} catch (SQLException sql) {
-			throw new DAOException(sql);
-		}
-		
-	
+		}catch (SQLException sql) {
+
+		throw new DAOException(sql);
 	}
+
 }
--- project/src/main/java/com/betacom/controller/VisualizzaReport.java
+++ project/src/main/java/com/betacom/controller/VisualizzaReport.java
@@ -1,19 +1,36 @@
 package com.betacom.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Vector;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.betacom.architecture.dao.DAOConstants;
 
 @WebServlet("/inseriscireport")
-public class VisualizzaReport extends HttpServlet {
+public class VisualizzaReport extends HttpServlet implements DAOConstants {
 	private static final long serialVersionUID = 73737978172721L;
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	}
+	private Connection conn;
+	private Statement stmt;
 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			response.sendRedirect("visualizzareport.jsp");
+		}
+	}
 }
